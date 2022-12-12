a62d05ea61c746901a2c4486508c3bcce57e1b5c
Deadylllko <114492976+Deadylllko@users.noreply.github.com>
2022-12-12 12:58:56 +0300

    Add files via upload

---
+++ Занятие-3/task3(5).py
@@ -0,0 +1,13 @@
+def minimum(x,y,z):
+    if x < y and x < z:
+        return x
+    elif y < x and y < z:
+        return y
+    elif z < x and z < y:
+        return z
+
+a = int(input("Enter the first number:"))
+b = int(input("Enter the second number:"))
+c = int(input("Enter the third number:"))
+
+print("Min number:", minimum(a,b,c))
---
+++ Занятие-3/task3.py
@@ -0,0 +1,8 @@
+def sum (x, y, z):
+    return x + y + z
+
+x = int(input(" x = "))
+y = int(input(" y = "))
+z = int(input(" z = "))
+
+print("Сумма = ", sum(x,y,z))
